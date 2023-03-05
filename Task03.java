b61737b8f7b4bb8e842b6aebc221a1caaf370856
Goyhman Peter <pgoyhman@mail.ru>
2023-03-05 22:04:06 +0300

    task03

--- Task03.java
+++ Task03.java
@@ -5,8 +5,8 @@ public class Task03 {
         int number;
         for (int i = 2; i <= 1000; i++) {
             int count = 0;
-            for(int j =2;j<= i && count < 2;j++){
-                if(i%j==0){
+            for (int j = 2; j <= i && count < 2; j++) {
+                if (i % j == 0) {
                     count++;
                 }
             }
